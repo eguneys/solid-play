729a1bd352fbb4bae1db37a859f7a9735819f2b9
Emre Guneyler <eguneys@hotmail.com>
2022-09-21 12:17:12 +0000

    solid play

---
+++ src/main-app.tsx
@@ -0,0 +1,12 @@
+import { createSignal, createMemo } from 'solid-js'
+
+const App = () => {
+
+  return (<>
+    Hello
+      </>)
+}
+
+
+
+export default App
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import solidPlugin from 'vite-plugin-solid'
 
 export default defineConfig({
+  plugins: [
+    solidPlugin({ hot: false }),
+  ],
   build: {
     sourcemap: 'inline',
     lib: {
